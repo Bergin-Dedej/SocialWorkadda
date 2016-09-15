617a9c1775c5c391e950971f65926d76af4f509b
dontworrydawg <bergind777@gmail.com>
2016-09-15 09:03:00 -0400

    pushed some shit

--- SocialWorkadda/AppDelegate.m
+++ SocialWorkadda/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    // some shit to push
     return YES;
 }
 
